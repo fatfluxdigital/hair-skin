0e2589cf617d5f278169382711fe100dcf820dcb
fatfluxdigital <fatfluxdigital@gmail.com>
2023-06-19 01:17:56 +0530

    ignore ts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  }
-module.exports = nextConfig
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+module.exports = nextConfig;
